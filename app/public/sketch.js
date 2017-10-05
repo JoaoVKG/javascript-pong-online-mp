a362240671034690ba41f4f689248256d15c28d6
JoaoVKG <joao99vkg@gmail.com>
2017-10-05 11:17:17 -0300

    multiplayer online working

---
+++ app/public/sketch.js
@@ -0,0 +1,232 @@
+var screenHeight = 800;
+var screenWidth = 1600;
+var font;
+var Ball = {
+    sprite: '',
+    xspeed: 0,
+    yspeed: 0
+}
+
+var Controls = {
+    up1: 87,
+    down1: 83,
+    up2: 38,
+    down2: 40
+}
+
+var Sounds = {
+    hitpaddle: '',
+    hitwall: '',
+    opponentpoint: ''
+}
+
+var Paddle1 = {
+    sprite: ''
+}
+
+var Paddle2 = {
+    sprite: ''
+}
+
+var Score1 = {
+    value: 0
+}
+
+var Score2 = {
+    value: 0
+}
+
+function preload() {
+    font = loadFont('fonts/PressStart2P.ttf');
+    Sounds.hitpaddle = loadSound('sounds/hitpaddle.wav');
+    Sounds.hitwall = loadSound('sounds/hitwall.wav');
+    Sounds.opponentpoint = loadSound('sounds/opponentpoint.wav');
+}
+
+function setup() {
+    createCanvas(screenWidth, screenHeight);
+    createPaddles();
+    resetBall();
+}
+
+function draw() {
+    background(0);
+    createNet();
+    createScores();
+    drawSprites();
+    if (canMove) {
+        movePaddles();
+        if (paddle1MovedUp == true) {
+            moveUpServer(Paddle1);
+        }
+        if (paddle1MovedDown == true) {
+            moveDownServer(Paddle1);
+        }
+        if (paddle1MovedUp == false && paddle1MovedDown == false) {
+            stopPaddleServer(Paddle1);
+        }
+
+        if (paddle2MovedUp == true) {
+            moveUpServer(Paddle2);
+        }
+        if (paddle2MovedDown == true) {
+            moveDownServer(Paddle2);
+        }
+        if (paddle2MovedUp == false && paddle2MovedDown == false) {
+            stopPaddleServer(Paddle2);
+        }
+    }
+    ballMovement();
+    touchBall();
+    score();
+}
+
+function resetBall() {
+    createBall();
+}
+
+function createNet() {
+    fill(255);
+    noStroke();
+    rect(screenWidth/2 - 4, 0, 8, screenHeight);
+}
+
+function createBall() {
+    Ball.sprite = createSprite(screenWidth/2, screenHeight/2, 16, 16);
+    Ball.sprite.shapeColor = 255;
+}
+
+function createPaddles() {
+    Paddle1.sprite = createSprite(24, screenHeight/2, 16, 64);
+    Paddle1.sprite.shapeColor = 255;
+    Paddle1.sprite.setCollider('rectangle', 0, 0, 21, 69);
+
+    Paddle2.sprite = createSprite(screenWidth - 24, screenHeight/2, 16, 64);
+    Paddle2.sprite.shapeColor = 255;
+    Paddle2.sprite.setCollider('rectangle', 0, 0, 21, 69);
+}
+
+function createScores() {
+    textFont(font);
+    textSize(54);
+    textAlign(CENTER);
+    text(Score1.value, (screenWidth/2) - 80, 75);
+    text(Score2.value, (screenWidth/2) + 80, 75);
+}
+
+function ballMovement() {
+    if (Ball.sprite.position.y - 16 < 0 || Ball.sprite.position.y + 16 > screenHeight) {
+        Sounds.hitwall.play();
+        Ball.yspeed = -Ball.yspeed;
+    }
+    Ball.sprite.setVelocity(Ball.xspeed, Ball.yspeed);
+}
+
+function score() {
+    if (Ball.sprite.position.x - 16 < 0) {
+        Sounds.opponentpoint.play();
+        Score2.value++;
+        resetBall();
+    }
+    if (Ball.sprite.position.x + 16 > screenWidth) {
+        Sounds.opponentpoint.play();
+        Score1.value++;
+        resetBall();
+    }
+}
+
+function movePaddles() {
+    if (admin) {
+        moveUp(Paddle1, Controls.up1);
+        moveDown(Paddle1, Controls.down1);
+    } else {
+        moveUp(Paddle2, Controls.up2);
+        moveDown(Paddle2, Controls.down2);  
+    }
+}
+
+function moveUpServer(paddle) {
+    if (paddle.sprite.position.y - 32 > 0) {
+        paddle.sprite.setVelocity(0, -4);
+    } else {
+        paddle.sprite.setVelocity(0, 0);
+    }
+}
+
+function moveDownServer(paddle) {
+    if (paddle.sprite.position.y + 32 < screenHeight) {
+        paddle.sprite.setVelocity(0, 4);
+    } else {
+        paddle.sprite.setVelocity(0, 0);
+    } 
+}
+
+function stopPaddleServer(paddle) {
+    paddle.sprite.setVelocity(0, 0);
+}
+
+function moveUp(paddle, control) {
+    if (keyDown(control)) {
+        if (paddle == Paddle1) {
+            socket.emit('paddle1MovedUpClient');
+        }
+        if (paddle == Paddle2) {
+            socket.emit('paddle2MovedUpClient');
+        }
+        if (paddle.sprite.position.y - 32 > 0) {
+            paddle.sprite.setVelocity(0, -4);
+        } else {
+            paddle.sprite.setVelocity(0, 0);
+        } 
+    }
+    if (keyWentUp(control)) {
+        if (paddle == Paddle1) {
+            socket.emit('paddle1StoppedClient');
+        }
+        if (paddle == Paddle2) {
+            socket.emit('paddle2StoppedClient');
+        }
+        paddle.sprite.setVelocity(0, 0);
+    }
+}
+
+function moveDown(paddle, control) {
+    if (keyDown(control)) {
+        if (paddle == Paddle1) {
+            socket.emit('paddle1MovedDownClient');
+        }
+        if (paddle == Paddle2) {
+            socket.emit('paddle2MovedDownClient');
+        }
+        if (paddle.sprite.position.y + 32 < screenHeight) {
+            paddle.sprite.setVelocity(0, 4);
+        } else {
+            paddle.sprite.setVelocity(0, 0);
+        }
+    }
+    if (keyWentUp(control)) {
+        if (paddle == Paddle1) {
+            socket.emit('paddle1StoppedClient');
+        }
+        if (paddle == Paddle2) {
+            socket.emit('paddle2StoppedClient');
+        }
+        paddle.sprite.setVelocity(0, 0);
+    }
+}
+
+function touchBall() {
+    Ball.sprite.overlap(Paddle1.sprite, function() {
+        Sounds.hitpaddle.play();
+        Ball.xspeed = -Ball.xspeed;
+        Ball.sprite.setVelocity(Ball.xspeed, Ball.yspeed);   
+    });
+    Ball.sprite.overlap(Paddle2.sprite, function() {
+        Sounds.hitpaddle.play();
+        Ball.xspeed = -Ball.xspeed;
+        Ball.sprite.setVelocity(Ball.xspeed, Ball.yspeed);
+    });
+}
+
+
+
--- index.js
+++ index.js
@@ -23,6 +23,15 @@ const server = app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor está online na porta " + (process.env.PORT || 3000));
 })
 
+// GAME
+
+var BallServer = {
+    xspeed: 0,
+    yspeed: 0
+}
+
+// GAME
+
 const io = require('socket.io').listen(server);
 
 app.set('io', io);
@@ -42,11 +51,54 @@ io.on('connection', function(socket) {
             if (nspPlayers[data.nsp] < 2) {
                 console.log('someone connected at ' + data.nsp);
                 nspPlayers[data.nsp]++;
-                socket.on('click', function() {
-                    socket.broadcast.emit('clicked');
+            
+                // SERVER RESPONSE TO CLIENT GAME
+                socket.on('paddle1MovedUpClient', function() {
+                    socket.emit('paddle1MovedUpServer');
+                    socket.broadcast.emit('paddle1MovedUpServer');
                 })
-                if (nspPlayers[data.nsp] == 2) {
 
+                socket.on('paddle1MovedDownClient', function() {
+                    socket.emit('paddle1MovedDownServer');
+                    socket.broadcast.emit('paddle1MovedDownServer');
+                })
+
+                socket.on('paddle1StoppedClient', function() {
+                    socket.emit('paddle1StoppedServer');
+                    socket.broadcast.emit('paddle1StoppedServer');
+                })
+
+
+                socket.on('paddle2MovedUpClient', function() {
+                    socket.emit('paddle2MovedUpServer');
+                    socket.broadcast.emit('paddle2MovedUpServer');
+                })
+
+                socket.on('paddle2MovedDownClient', function() {
+                    socket.emit('paddle2MovedDownServer');
+                    socket.broadcast.emit('paddle2MovedDownServer');
+                })
+
+                socket.on('paddle2StoppedClient', function() {
+                    socket.emit('paddle2StoppedServer');
+                    socket.broadcast.emit('paddle2StoppedServer');
+                })
+
+                if (nspPlayers[data.nsp] == 1) {
+                    socket.emit('isAdmin', {
+                        admin: true
+                    })
+                }
+                if (nspPlayers[data.nsp] == 2) {
+                    resetBallServer();
+                    socket.emit('gameStarted', {
+                        BallServer: BallServer,
+                        canMove: true
+                    });
+                    socket.broadcast.emit('gameStarted', {
+                        BallServer: BallServer,
+                        canMove: true
+                    });
                 }
             } else {
                 socket.emit('serverFull');
@@ -55,3 +107,8 @@ io.on('connection', function(socket) {
     })
 
 })
+
+function resetBallServer() {
+    BallServer.xspeed = Math.random() < 0.5 ? 5 : -5;
+    BallServer.yspeed = Math.random() < 0.5 ? 5 : -5;
+}
